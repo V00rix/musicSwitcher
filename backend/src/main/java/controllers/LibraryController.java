223be0e8dd98c551003535aaae200108a345eec2
v00rix <vladogim97@gmail.com>
2018-04-23 08:29:38 +0200

    Songs component work in progress, basic sorting by title

--- backend/src/main/java/controllers/LibraryController.java
+++ backend/src/main/java/controllers/LibraryController.java
@@ -2,7 +2,11 @@ package controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import domain.AudioFile;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Library controller
@@ -19,6 +23,9 @@ public class LibraryController extends ControllerBase {
     String library() {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
+            this.libraryProvider.getFiles().sort((a1, a2) -> {
+                return (a1.title == null) ? ((a2.title == null) ? 1 : -1) : ((a2.title == null) ? 1 : a1.title.compareTo(a2.title));
+            });
             return objectMapper.writeValueAsString(this.libraryProvider.getFiles());
         } catch (JsonProcessingException e) {
             e.printStackTrace();
